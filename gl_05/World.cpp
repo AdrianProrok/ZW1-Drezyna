3f756a275a38fd866822f84c31d82e50707f59ba
gwypych <gwypych@mion.elka.pw.edu.pl>
2019-01-13 19:19:19 +0100

    Light control for each light

--- gl_05/World.cpp
+++ gl_05/World.cpp
@@ -65,20 +65,49 @@ void World::render()
 
 void World::update(float delta_time, const Input& input)
 {	
+	static int light_index = 0;
+	static int light_sens = 100;
+
+	if (input.keyboard.keys_pressed[GLFW_KEY_0])
+		light_index = 0;
+	if (input.keyboard.keys_pressed[GLFW_KEY_1])
+		light_index = 1;
+	if (input.keyboard.keys_pressed[GLFW_KEY_2])
+		light_index = 2;
+	if (input.keyboard.keys_pressed[GLFW_KEY_3])
+		light_index = 3;
+	if (input.keyboard.keys_pressed[GLFW_KEY_4])
+		light_index = 0;
+	if (input.keyboard.keys_pressed[GLFW_KEY_5])
+		light_index = 0;
+	if (input.keyboard.keys_pressed[GLFW_KEY_6])
+		light_index = 0;
+	if (input.keyboard.keys_pressed[GLFW_KEY_7])
+		light_index = 0;
+	if (input.keyboard.keys_pressed[GLFW_KEY_8])
+		light_index = 0;
+	if (input.keyboard.keys_pressed[GLFW_KEY_9])
+		light_index = 0;
+
+	if (light_index == 0)
+		light_sens = 1000000;
+	else
+		light_sens = 100;
+
 	if (input.keyboard.keys_pressed[GLFW_KEY_U])
 		ambientStrength -= delta_time * 0.5f;
 	if (input.keyboard.keys_pressed[GLFW_KEY_I])
 		ambientStrength += delta_time * 0.5f;
 	if (input.keyboard.keys_pressed[GLFW_KEY_J])
-		light[0].intensity -= delta_time * 1000000;
+		light[light_index].intensity -= delta_time * light_sens;
 	if (input.keyboard.keys_pressed[GLFW_KEY_K])
-		light[0].intensity += delta_time * 1000000;
+		light[light_index].intensity += delta_time * light_sens;
 	if (input.keyboard.keys_pressed[GLFW_KEY_H]) {
 		light[0].intensity = 3000000.0f;
 	}
 
-	if (light[0].intensity < 0)
-		light[0].intensity = 0;
+	if (light[light_index].intensity < 0)
+		light[light_index].intensity = 0;
 	if (ambientStrength < 0)
 		ambientStrength = 0;
 
